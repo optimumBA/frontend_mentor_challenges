1ca8f7bb087a22dc81bd3d1f736ec6e90ca423a0
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-15 13:50:00 +0100

    Prepare QR code component challenge

---
+++ test/specs/qr_code_component.e2e.js
@@ -0,0 +1,15 @@
+describe('QR code component', () => {
+  it('should open page', async () => {
+    await browser.url(`http://localhost:4567/qr_code_component`)
+
+    await expect(browser).toHaveTitle(
+      'QR code component • Frontend Mentor Challenges • Optimum BH Internship'
+    )
+    await expect($('body')).toHaveTextContaining('Frontend Mentor challenge')
+    await expect($('a')).toHaveHref(
+      'https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H'
+    )
+    await expect($('body')).toHaveTextContaining('for Optimum BH Internship')
+    await expect($('body')).toHaveTextContaining('Source code')
+  })
+})
