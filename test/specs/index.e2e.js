620a3842188cd87e451e0fcefe9df2338908e84f
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-15 10:09:59 +0100

    Fix formatting

--- test/specs/index.e2e.js
+++ test/specs/index.e2e.js
@@ -2,6 +2,8 @@ describe('Index', () => {
   it('should open page', async () => {
     await browser.url(`http://localhost:4567`)
 
-    await expect($('title')).toHaveTextContaining('Frontend Mentor Challenges • Optimum BH Internship')
+    await expect($('title')).toHaveTextContaining(
+      'Frontend Mentor Challenges • Optimum BH Internship'
+    )
   })
 })
