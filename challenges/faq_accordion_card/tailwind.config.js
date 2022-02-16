20d344eacaf0fda96395d96288529527ea66383f
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-16 18:00:39 +0100

    Improve FAQ accordion card challenge
    Fix credits position issue.
    Fix Tailwind CSS config.
    Improve test to save screenshot for two viewport sizes.

--- challenges/faq_accordion_card/tailwind.config.js
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   content: ['./challenges/faq_accordion_card/index.html'],
   theme: {
-    colors: {
-      'optimum-blue': '#009efc',
-      'optimum-darkblue': '#0389e1',
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+      },
     },
   },
   plugins: [],
