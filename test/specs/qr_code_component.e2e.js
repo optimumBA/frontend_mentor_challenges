1d9806d75c68ef90d4afe57325865afe5c5d2c30
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-16 10:28:43 +0100

    Fix QR code component test
    Avoid potential issues with multiple links on the page.

--- test/specs/qr_code_component.e2e.js
+++ test/specs/qr_code_component.e2e.js
@@ -6,10 +6,13 @@ describe('QR code component', () => {
       'QR code component • Frontend Mentor Challenges • Optimum BH Internship'
     )
     await expect($('body')).toHaveTextContaining('Frontend Mentor challenge')
-    await expect($('a')).toHaveHref(
+    await expect($('=Frontend Mentor challenge')).toHaveHref(
       'https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H'
     )
     await expect($('body')).toHaveTextContaining('for Optimum BH Internship')
     await expect($('body')).toHaveTextContaining('Source code')
+    await expect($('=Source code')).toHaveHrefContaining(
+      '/frontend_mentor_challenges/tree/qr_code_component/challenges/qr_code_component'
+    )
   })
 })
