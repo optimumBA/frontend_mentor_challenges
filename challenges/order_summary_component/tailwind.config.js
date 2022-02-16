4a818b423a2128e885df59d2a4e8088a600d1eee
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-16 17:31:41 +0100

    Improve Order summary component challenge
    Fix credits position issue.
    Fix Tailwind CSS config.
    Improve test to save screenshot for two viewport sizes.

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   content: ['./challenges/order_summary_component/index.html'],
   theme: {
-    colors: {
-      'optimum-blue': '#009efc',
-      'optimum-darkblue': '#0389e1',
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+      },
     },
   },
   plugins: [],
