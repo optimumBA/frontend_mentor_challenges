3fa44e80f73bc803b92cfa9b3c4003c81f6e234e
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-16 10:16:18 +0100

    Prepare FAQ accordion card challenge

---
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./challenges/faq_accordion_card/index.html'],
+  theme: {
+    colors: {
+      'optimum-blue': '#009efc',
+      'optimum-darkblue': '#0389e1',
+    },
+  },
+  plugins: [],
+  variants: {},
+}
---
+++ test/specs/faq_accordion_card.e2e.js
@@ -0,0 +1,64 @@
+describe('FAQ accordion card', () => {
+  it('should open page and toggle questions', async () => {
+    await browser.url(`http://localhost:4567/faq_accordion_card`)
+
+    await expect(browser).toHaveTitle(
+      'FAQ accordion card • Frontend Mentor Challenges • Optimum BH Internship'
+    )
+    await expect($('body')).toHaveTextContaining('Frontend Mentor challenge')
+    await expect($('=Frontend Mentor challenge')).toHaveHref(
+      'https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam'
+    )
+    await expect($('body')).toHaveTextContaining('for Optimum BH Internship')
+    await expect($('body')).toHaveTextContaining('Source code')
+    await expect($('=Source code')).toHaveHrefContaining(
+      '/frontend_mentor_challenges/tree/faq_accordion_card/challenges/faq_accordion_card'
+    )
+
+    await expect($('body')).toHaveTextContaining(
+      'How many team members can I invite?'
+    )
+    await expect($('body')).toHaveTextContaining(
+      'What is the maximum file upload size?'
+    )
+    await expect($('body')).toHaveTextContaining('How do I reset my password?')
+    await expect($('body')).toHaveTextContaining(
+      'Can I cancel my subscription?'
+    )
+    await expect($('body')).toHaveTextContaining(
+      'Do you provide additional support?'
+    )
+
+    await expect($('body')).toHaveTextContaining(
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+    )
+    await expect($('body')).not.toHaveTextContaining(
+      'No more than 2GB. All files in your account must fit your allotted storage space.'
+    )
+    await expect($('body')).not.toHaveTextContaining(
+      'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'
+    )
+
+    await $('=What is the maximum file upload size?').click()
+    await expect($('body')).not.toHaveTextContaining(
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+    )
+    await expect($('body')).toHaveTextContaining(
+      'No more than 2GB. All files in your account must fit your allotted storage space.'
+    )
+    await expect($('body')).not.toHaveTextContaining(
+      'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'
+    )
+
+    await $('=How do I reset my password?').click()
+    await expect($('body')).not.toHaveTextContaining(
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+    )
+    await expect($('body')).not.toHaveTextContaining(
+      'No more than 2GB. All files in your account must fit your allotted storage space.'
+    )
+    await expect($('body')).toHaveTextContaining(
+      'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'
+    )
+  })
+})
