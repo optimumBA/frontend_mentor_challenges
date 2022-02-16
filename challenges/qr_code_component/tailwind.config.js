e6a5b362c5c8cb50d33094faee4f35038b92f825
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-16 16:02:59 +0100

    Improve QR code component challenge
    Fix Tailwind CSS config.
    Improve test to save screenshot for two viewport sizes.

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   content: ['./challenges/qr_code_component/index.html'],
   theme: {
-    colors: {
-      'optimum-blue': '#009efc',
-      'optimum-darkblue': '#0389e1',
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+      },
     },
   },
   plugins: [],
