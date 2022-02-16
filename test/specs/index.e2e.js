131ea1b4626535f57baa5026b2b04bd6a83e7706
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-02-16 15:24:00 +0100

    Improve tests

--- test/specs/index.e2e.js
+++ test/specs/index.e2e.js
@@ -8,11 +8,19 @@ describe('Index', () => {
       'Frontend Mentor Challenges • Optimum BH Internship'
     )
     await expect($('body')).toHaveTextContaining(
-      'Front-end development Challenges'
+      'Front-end development challenges'
     )
     await expect($('body')).toHaveTextContaining('by Frontend Mentor')
-    await expect($('body')).toHaveTextContaining('for Optimum BH Internship.')
+    await expect($('=Frontend Mentor')).toHaveHref(
+      'https://www.frontendmentor.io'
+    )
+    await expect($('body')).toHaveTextContaining('for Optimum BH Internship')
+    await expect($('=Optimum BH')).toHaveHref('https://github.com/optimumBA')
     await expect($('body')).toHaveTextContaining('Source code')
+    await expect($('=Source code')).toHaveHrefContaining('https://github.com')
+    await expect($('=Source code')).toHaveHrefContaining(
+      '/frontend_mentor_challenges'
+    )
   })
 
   it('should open page in mobile viewport size', async () => {
@@ -25,10 +33,18 @@ describe('Index', () => {
       'Frontend Mentor Challenges • Optimum BH Internship'
     )
     await expect($('body')).toHaveTextContaining(
-      'Front-end development Challenges'
+      'Front-end development challenges'
     )
     await expect($('body')).toHaveTextContaining('by Frontend Mentor')
-    await expect($('body')).toHaveTextContaining('for Optimum BH Internship.')
+    await expect($('=Frontend Mentor')).toHaveHref(
+      'https://www.frontendmentor.io'
+    )
+    await expect($('body')).toHaveTextContaining('for Optimum BH Internship')
+    await expect($('=Optimum BH')).toHaveHref('https://github.com/optimumBA')
     await expect($('body')).toHaveTextContaining('Source code')
+    await expect($('=Source code')).toHaveHrefContaining('https://github.com')
+    await expect($('=Source code')).toHaveHrefContaining(
+      '/frontend_mentor_challenges'
+    )
   })
 })
