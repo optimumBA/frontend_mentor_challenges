b7fa727f43b8c6d5f5b64295b918ab7a3d214682
adnamaric <adna.maric@edu.fit.ba>
2022-03-14 22:37:03 +0100

    Add QR Code Component

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        lightgray: '#D6E2F0',
+        grayishgray: '#7B879D',
+        darkblue: '#1f3251',
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+        fontFamily: {
+          Outfit: ['Outfit'],
+        },
+        screens: {
+          sm: '375px',
+          // => @media (min-width: 640px) { ... }
+
+          lg: '1440px',
+          // => @media (min-width: 1024px) { ... }
+        },
       },
     },
   },
