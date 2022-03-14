c1472749afab564aa5e03c6b042c7e5256d28c14
Azryel <AzryelShroud@gmail.com>
2022-03-14 19:18:22 +0100

    Add qr code component

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+
+        "lightGray": "hsl(212, 45%, 89%)",
+        "grayishBlue": "hsl(220, 15%, 55%)",
+        "darkBlue": "hsl(218, 44%, 22%)"
+      },
+      fontFamily: {
+        "rubik": ["Rubik"],
       },
     },
   },
