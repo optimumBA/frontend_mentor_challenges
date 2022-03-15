03d50b1017158373253b248454dde06e3e958e2f
adnamaric <adna.maric@edu.fit.ba>
2022-03-15 18:40:40 +0100

    Add Stats preview card component

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -1,13 +1,38 @@
 module.exports = {
-  content: ['./challenges/stats_preview_card_component/index.html'],
+  content: ['./*html'],
   theme: {
     extend: {
-      colors: {
-        'optimum-blue': '#009efc',
-        'optimum-darkblue': '#0389e1',
+      colors:{
+        'verydarkblue' : '#090b1a',
+         'darkdesaturedblue': '#1b1938',
+         'softviolet':'#aa5cdb',
+         'white':'#ffffff',
+         'slightlytrwhite75' : '#ffffffbf',
+         'slightlytrwhite65':'#ffffff99',
+         'optimum-blue': '#009efc',
+         'optimum-darkblue': '#0389e1',
       },
+      
+      fontFamily:{
+        'Inter':['Inter'],
+        'LexendDeca':['Lexend Deca']
+      },
+      backgroundImage:{
+        'mobile':"url(./images/image-header-mobile.jpg)",
+        'desktop':"url(../images/image-header-desktop.jpg)",
+      },
+      screens: {
+        'sm': '375px',
+        'md': '768px',
+      // => @media (min-width: 768px) { ... }
+  
+        'lg': '1440px',
+        // => @media (min-width: 1024px) { ... }
+        
+      }
     },
   },
   plugins: [],
-  variants: {},
 }
+
+
