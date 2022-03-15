974e3e3d3dd119b2f79a847c9f5d38fc07b208a3
Azryel <AzryelShroud@gmail.com>
2022-03-15 20:57:15 +0100

    Add order summary component

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+
+        paleBlue: 'hsl(225, 100%, 94%)',
+        brightBlue: 'hsl(245, 75%, 52%)',
+        veryPaleBlue: 'hsl(225, 100%, 98%)',
+        desaturatedBlue: 'hsl(224, 23%, 55%)',
+        darkBlue: 'hsl(223, 47%, 23%)',
+      },
+      fontFamily: {
+        redHat: ['Red Hat Display'],
+      },
+      backgroundImage: {
+        mobilePattern: "url('./images/pattern-background-mobile.svg')",
+        desktopPattern: "url('./images/pattern-background-desktop.svg')",
       },
     },
   },
