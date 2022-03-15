7dfb28baabedba7e1c968b4eee5e6654099a647e
adnamaric <adna.maric@edu.fit.ba>
2022-03-15 18:52:28 +0100

    CI & CD test fix.

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -2,37 +2,34 @@ module.exports = {
   content: ['./*html'],
   theme: {
     extend: {
-      colors:{
-        'verydarkblue' : '#090b1a',
-         'darkdesaturedblue': '#1b1938',
-         'softviolet':'#aa5cdb',
-         'white':'#ffffff',
-         'slightlytrwhite75' : '#ffffffbf',
-         'slightlytrwhite65':'#ffffff99',
-         'optimum-blue': '#009efc',
-         'optimum-darkblue': '#0389e1',
+      colors: {
+        verydarkblue: '#090b1a',
+        darkdesaturedblue: '#1b1938',
+        softviolet: '#aa5cdb',
+        white: '#ffffff',
+        slightlytrwhite75: '#ffffffbf',
+        slightlytrwhite65: '#ffffff99',
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
       },
-      
-      fontFamily:{
-        'Inter':['Inter'],
-        'LexendDeca':['Lexend Deca']
+
+      fontFamily: {
+        Inter: ['Inter'],
+        LexendDeca: ['Lexend Deca'],
       },
-      backgroundImage:{
-        'mobile':"url(./images/image-header-mobile.jpg)",
-        'desktop':"url(../images/image-header-desktop.jpg)",
+      backgroundImage: {
+        mobile: 'url(./images/image-header-mobile.jpg)',
+        desktop: 'url(../images/image-header-desktop.jpg)',
       },
       screens: {
-        'sm': '375px',
-        'md': '768px',
-      // => @media (min-width: 768px) { ... }
-  
-        'lg': '1440px',
+        sm: '375px',
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1440px',
         // => @media (min-width: 1024px) { ... }
-        
-      }
+      },
     },
   },
   plugins: [],
 }
-
-
