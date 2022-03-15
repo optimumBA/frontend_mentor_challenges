b6d317a1f6e91507f63c7b9759391f07975eb64b
Azryel <AzryelShroud@gmail.com>
2022-03-15 17:32:29 +0100

    Run prettier

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -6,16 +6,16 @@ module.exports = {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
 
-        "very-dark-blue": "hsl(233, 47%, 7%)",
-        "darkDesaturatedBlue": "hsl(244, 38%, 16%)",
-        "softViolet": "hsl(277, 64%, 61%)",
-        "mainPara": "hsla(0, 0%, 100%, 0.75)",
-        "stats": "hsla(0, 0%, 100%, 0.6)"
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        darkDesaturatedBlue: 'hsl(244, 38%, 16%)',
+        softViolet: 'hsl(277, 64%, 61%)',
+        mainPara: 'hsla(0, 0%, 100%, 0.75)',
+        stats: 'hsla(0, 0%, 100%, 0.6)',
       },
       fontFamily: {
-        "inter": ["Inter"],
-        "lexend": ["Lexend+Deca"]
-      }
+        inter: ['Inter'],
+        lexend: ['Lexend+Deca'],
+      },
     },
   },
   plugins: [],
