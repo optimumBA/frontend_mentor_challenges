1383e5c4268939150d5d84550f4b73d718169980
adnamaric <adna.maric@edu.fit.ba>
2022-03-16 13:00:30 +0100

    Add order summary component

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -1,13 +1,34 @@
 module.exports = {
-  content: ['./challenges/order_summary_component/index.html'],
+  content: ['./*html'],
   theme: {
     extend: {
+      fontFamily: {
+        'red-hat-display': ['Red Hat Display', 'sans-serif'],
+      },
       colors: {
-        'optimum-blue': '#009efc',
-        'optimum-darkblue': '#0389e1',
+        'bright-blue': '#e0e8ff',
+        'pale-blue': '#3829e0',
+        'very-pale-blue': '#f5f7ff',
+        'desatured-blue': '#7280a7',
+        'dark-blue': '#1f2f56',
+        'req-blue': ' #382ae1',
+      },
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1440px',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('images/pattern-background-desktop.svg')",
+        'footer-texture': "url('images/pattern-background-mobile.svg')",
+      },
+      width: {
+        400: '23rem',
+        350: '20rem',
+        '85p': '85%',
+        '350p': '21.8rem',
       },
     },
   },
   plugins: [],
-  variants: {},
 }
