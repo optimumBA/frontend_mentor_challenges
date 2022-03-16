42e94945ddd580e75e3fc8bd3a529e4687931390
adnamaric <adna.maric@edu.fit.ba>
2022-03-16 09:25:55 +0100

    Add profile card component

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-  content: ['./challenges/profile_card_component/index.html'],
+  content: ['./*html'],
   theme: {
     extend: {
       colors: {
+        darkcyan: '#19a2ae',
+        verydarkblue: '#2d3248',
+        darkgrayishblue: '#6a6f81',
+        darkgray: '#969696',
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+      },
+      fontFamily: {
+        KumbhSans: ['Kubmh Sans'],
       },
     },
   },
   plugins: [],
-  variants: {},
 }
