adc3f0e23d295c4f983ac95791b83a38e64a1100
Nedim <nedimkarcic52@gmail.com>
2022-03-16 12:07:38 +0100

    Add tailwind configurations

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -5,9 +5,29 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        white: 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)',
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+      screens: {
+        sm: '375px',
+        // => @media (min-width: 576px) { ... }
+  
+        md: '960px',
+        // => @media (min-width: 960px) { ... }
+  
+        lg: '1440px',
+        // => @media (min-width: 1440px) { ... }
+  
+        tv: '2560px',
+        // => @media (min-width: 2560px) { ... }
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss'), require('autoprefixer')],
   variants: {},
 }
