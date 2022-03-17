8168743081cab294bd847d1f5531464bd22bee3e
adnamaric <adna.maric@edu.fit.ba>
2022-03-17 21:04:09 +0100

    Add Chat app css illustration

--- challenges/chat_app_css_illustration/tailwind.config.js
+++ challenges/chat_app_css_illustration/tailwind.config.js
@@ -1,13 +1,58 @@
 module.exports = {
-  content: ['./challenges/chat_app_css_illustration/index.html'],
+  content: ['./*html'],
   theme: {
     extend: {
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1440px',
+      },
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'pale-violet': 'hsl(276, 100%, 81%)',
+        'moderate-violet': 'hsl(276, 55%, 52%)',
+        'desaturated-dark-violet': 'hsl(271, 15%, 43%)',
+        'grayish-blue': 'hsl(206, 6%, 79%)',
+        'very-dark-desaturated-violet': 'hsl(271, 36%, 24%)',
+        'dark-grayish-violet': 'hsl(270, 7%, 64%)',
+        'light-magenta': 'hsl(293, 100%, 63%)',
+        'light-violet': 'hsl(264, 100%, 61%)',
+        White: 'hsl(0, 0%, 100%)',
+        'light-grayish-violet': 'hsl(270, 20%, 96%)',
+        'very-dark-desaturated-violet': 'hsl(271, 36%, 24%)',
+        'very-light-magenta': 'hsl(289, 100%, 72%)',
+      },
+      fontFamily: {
+        Rubik: ['Rubik', 'sans-serif'],
+      },
+      width: {
+        240: '240px',
+        230: '230px',
+        'phone-w': '280px',
+        240: '240px',
+        'phone-header': '260px',
+        booking: '540px',
+        'bg-width': '700px',
+      },
+      height: {
+        496: '496px',
+        'phone-h': '530px',
+        'bg-height': '600px',
+      },
+      borderWidth: {
+        10: '10px',
+      },
+      margin: {
+        '298px': '-294px',
+        5: '5px',
+        440: '-440px',
+        '39rem': '39rem',
+      },
+      fontSize: {
+        'text-xxs': '.5rem',
       },
     },
   },
   plugins: [],
-  variants: {},
 }
