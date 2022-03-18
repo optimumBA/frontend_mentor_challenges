0f63549e3a7bf8dcb20b2bc3045093d78d582c32
Nedim <nedimkarcic52@gmail.com>
2022-03-18 07:43:31 +0100

    Add changes to packages and solve conflicts

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -1,13 +1,26 @@
 module.exports = {
-  content: ['./challenges/profile_card_component/index.html'],
+  // content: ['./challenges/profile_card_component/index.html'],
+  content: ['./*.html'],
   theme: {
     extend: {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
+      },
+      screens: {
+        'vsm': '375px',
+      },
+      fontFamily: {
+        'kumbh': ['Kumbh Sans', 'sans-serif',]
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('autoprefixer'),
+  ],
   variants: {},
 }
