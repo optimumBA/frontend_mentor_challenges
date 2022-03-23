1e3dd6f51ea40d73d9d77b126bc4ab6bab85d164
ademirson <ademirson15@hotmail.com>
2022-03-23 13:52:50 +0100

    Adding Images to Folder

---
+++ challenges/qr_code_component/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ['./challenges/index.html'],
+    theme: {
+      extend: {
+        colors: {
+          'optimum-blue': '#009efc',
+          'optimum-darkblue': '#0389e1',
+        },
+      },
+    },
+    plugins: [],
+    variants: {},
+  }
