58243f76478d50506ace345b3e2234bf1090447d
Azryel <AzryelShroud@gmail.com>
2022-03-28 18:57:39 +0200

    Add faq accordion card

--- challenges/faq_accordion_card/tailwind.config.js
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+
+        //primary text
+        veryDarkDesaturatedBlue: 'hsl(238, 29%, 16%)',
+        softRed: 'hsl(14, 88%, 65%)',
+        //bg gradients
+        softViolet: 'hsl(273, 75%, 66%)',
+        softBlue: 'hsl(240, 73%, 65%)',
+        //neutral text
+        veryDarkGrayishBlue: 'hsl(237, 12%, 33%)',
+        darkGrayishBlue: 'hsl(240, 6%, 50%)',
+        //dividers
+        lightGrayishBlue: 'hsl(240, 5%, 91%)',
+      },
+      fontFamily: {
+        kumbh: ['Kumbh Sans'],
+      },
+      backgroundImage: {
+        mobilePattern: "url('./images/bg-pattern-mobile.svg')",
+        desktopPattern: "url('./images/bg-pattern-desktop.svg')",
       },
     },
   },
