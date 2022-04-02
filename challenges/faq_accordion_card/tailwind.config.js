a8e04afcee04d516e70a9274d70bdd4067d96116
adnamaric <adna.maric@edu.fit.ba>
2022-04-02 10:54:26 +0200

    Add faq_accordion_card

--- challenges/faq_accordion_card/tailwind.config.js
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -4,15 +4,24 @@ module.exports = {
     extend: {
       colors: {
         'optimum-blue': '#009efc',
-        'optimum-darkblue': '#0389e1'
+        'optimum-darkblue': '#0389e1',
+        'very-dark-desaturated-blue': 'hsl(238, 29%, 16%)',
+        'soft-red': 'hsl(14, 88%, 65%)',
+        'soft-violet': 'hsl(273, 75%, 66%)',
+        'soft-blue': 'hsl(240, 73%, 65%)',
+        'very-dark-grayish-blue': 'hsl(237, 12%, 33%)',
+        'dark-grayish-blue': 'hsl(240, 6%, 50%)',
+        'light-grayish-blue': 'hsl(240, 5%, 91%)',
+      },
+      backgroundPosition: {
+        'left-top': 'top 1rem left -2rem',
       },
       fontFamily: {
         'Kumbh Sans': ['Kumbh Sans'],
       },
       screens: {
         desktop: '1200px',
-        mobile: '370px',
-        lg: '1440px',
+        mobile: { min: '370px', max: '700px' },
       },
     },
   },
