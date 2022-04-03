0cf212bb4f8f3c7b9aeb70b7f2d14ab73d19b872
Daniel Molnar <daniel4molnar@gmail.com>
2022-04-03 22:27:08 +0200

    Add Interactive pricing component
    - Implement the optimal layout for the app on mobile, tablet and desktop devices
    - Users can see hover states and interact to change price value acording to slider ranges
    - Users can toggle montly/yearly option

--- challenges/interactive_pricing_component/tailwind.config.js
+++ challenges/interactive_pricing_component/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./challenges/interactive_pricing_component/index.html'],
   theme: {
@@ -5,6 +7,36 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        prim: {
+          'soft-cyan': '#a5f3eb' /* Full Slider Bar */,
+          'strong-cyan': '#10d5c2' /* Slider Backround */,
+          'light-grayish-red': '#feece7' /* Discount Background */,
+          'light-red': '#ff8c66' /* Discount Text */,
+          'pale-blue': '#bdccff' /* CTA Text */,
+        },
+        neutral: {
+          'very-pale-blue': '#fafbff' /* Main Background */,
+          'light-grayish-blue': '#eaeefb' /* Empty Slider Bar */,
+          'very-dark-black-blue1': '#cdd7ee' /* Toggle Background */,
+          'grayish-blue': '#858fad' /* Text */,
+          'dark-desat-blue': '#293356' /* Text & CTA Background */,
+        },
+      },
+      fontSize: {
+        'body-copy': '15px',
+      },
+      fontFamily: {
+        manrope: ['Manrope', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        /* Responsive breakpoints */ mobile: '375px',
+        // => @media (min-width: 375px) { ... }
+        tablet: '768px',
+        // => @media (min-width: 768px) { ... }
+        tablet2: '900px',
+        // => @media (min-width: 900px) { ... }
+        desktop: '1440px',
+        // => @media (min-width: 1440px) { ... }
       },
     },
   },
