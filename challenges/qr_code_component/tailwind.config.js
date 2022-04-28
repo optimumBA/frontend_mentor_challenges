bf386a03e979fbf5cf8d474ef0a999deaf7d20dd
oki-imamovic <oki.imamovic@gmail.com>
2022-04-28 19:34:33 +0200

    QR Code Component finished

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -1,10 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./challenges/qr_code_component/index.html'],
   theme: {
     extend: {
       colors: {
+        'mine-bg': '#d6e2f0',
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+      },
+      fontFamily: {
+        outfit: ['Outfit', ...defaultTheme.fontFamily.sans],
       },
     },
   },
