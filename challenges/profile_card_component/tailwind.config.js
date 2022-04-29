1ed36c2eff0ca290769d45861cde889a20051068
oki-imamovic <oki.imamovic@gmail.com>
2022-04-29 19:50:24 +0200

    Starting work on profile card component

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./challenges/profile_card_component/index.html'],
   theme: {
@@ -5,6 +6,21 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'dc': '#19a2ae',
+        'vddb': '#2d3248',
+        'dgb': '#6a6f81',
+        'dg': '#969696',
+      },
+      fontFamily: {
+        outfit: ['Outfit', ...defaultTheme.fontFamily.sans],
+        kumbh: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        '18': '18px'
+      },
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
       },
     },
   },
