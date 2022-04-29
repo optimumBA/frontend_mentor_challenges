08be8a200e10d02215600f631e9f32d22250e227
oki-imamovic <oki.imamovic@gmail.com>
2022-04-29 02:17:31 +0200

    Stats Preview desktop part done

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -1,10 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./challenges/stats_preview_card_component/index.html'],
   theme: {
     extend: {
+      fontFamily: {
+        Inter: ['Inter', ...defaultTheme.fontFamily.sans],
+        Deca: ['Lexend Deca', ...defaultTheme.fontFamily.sans],
+      },
+
+      fontSize: {
+        15: '15px',
+      },
+
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'very-dark-blue': '#090b1a',
+        'dark-desa': '#1b1938',
+        'soft-violet': '#aa5cdb',
+      },
+
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
       },
     },
   },
