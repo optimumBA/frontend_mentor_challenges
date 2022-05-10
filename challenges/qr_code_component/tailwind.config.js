456b210af9d97c7bd858dc18cbd7160a1fa142f7
OmerSalkan <salkanovicomer@gmail.com>
2022-05-10 11:47:08 +0200

    Add basic style and configuration

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': '#d6e2f0',
       },
+      fontSize: {
+        'text': '15px',
+      },
+      fontFamily: {
+        outfit: ['Outfit'],
+      },
+      screens: {
+        mobile: '376px',
+        tablet: '768px',
+        desktop: '1440px',
+      }
     },
   },
   plugins: [],
