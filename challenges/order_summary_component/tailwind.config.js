473b8d880f2bd965239faec92cccc2f08af321c7
OmerSalkan <salkanovicomer@gmail.com>
2022-05-12 20:02:50 +0200

    Added images and basic configuration

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)'
       },
+      fontFamily:{
+        redhat: ['Red Hat Display'],
+      }
     },
   },
   plugins: [],
