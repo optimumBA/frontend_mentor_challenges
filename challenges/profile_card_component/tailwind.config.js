cae1348ff585649239f93ac4a94c76a750af0d62
OmerSalkan <salkanovicomer@gmail.com>
2022-05-12 13:12:08 +0200

    Added images and style

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'dark-cyan' : 'hsl(185, 75%, 39%)',
+        'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
       },
+      fontSize:{
+        'card-text': '18px'
+      },
+      fontFamily: {
+        'kumbh-sans': ['Kumbh Sans'],
+      }
     },
   },
   plugins: [],
