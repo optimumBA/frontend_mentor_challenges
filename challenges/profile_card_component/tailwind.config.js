7808073761206186532e44eab03f9015bf6e3788
OmerSalkan <salkanovicomer@gmail.com>
2022-05-12 19:40:20 +0200

    Card completed

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -8,10 +8,13 @@ module.exports = {
         'dark-cyan' : 'hsl(185, 75%, 39%)',
         'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
         'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
+        
       },
       fontSize:{
         'card-text': '18px'
       },
+     
       fontFamily: {
         'kumbh-sans': ['Kumbh Sans'],
       }
