c2aa5ed263ff1e0554d8de16e20c1150dfa61537
senaid <senaid123>
2022-05-12 23:28:39 +0200

    Finished work on task
    -added proper screens to the components
    -added colors and responsiveness
    -removed unnecessary comonents

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        white: '	#ffffff',
+        'Light-gray': '	#d6e2f0',
+        'g-blue': '#7b879d',
+        'dark-blue': '#1f3251',
+      },
+      fontSize: {
+        sm: '15px',
       },
     },
   },
