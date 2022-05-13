262c5c0d4acf4392383747b88d5f01a151c129d8
Tarik Cahtarevic <tarikcahtarevic@gmail.com>
2022-05-13 17:05:52 +0200

    Finished building stats preview card component

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./challenges/stats_preview_card_component/index.html'],
   theme: {
@@ -5,6 +7,26 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'primary': {
+          1000: '#aa5cdb',
+          2000: '#090b1a',
+          3000: '#1b1938',
+        },
+        'neutral': {
+          1000: '#ffffff',
+          2000: 'rgba(255, 255, 255, 0.7)',
+          3000: 'rgba(255, 255, 255, 0.6)',
+        },
+      },
+      fontFamily: {
+        'inter': ['Inter', ...defaultTheme.fontFamily.sans],
+        'lexdec': ['Lexend Deca', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        mob: '375px',
+        // => @media (min-width: 375px) { ... }
+        desk: '1440px',
+        // => @media (min-width: 1440px) { ... }
       },
     },
   },
