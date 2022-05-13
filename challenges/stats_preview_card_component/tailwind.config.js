c27a645c97caa21c408bfb5d89f3d3284e5461ab
Tarik Cahtarevic <tarikcahtarevic@gmail.com>
2022-05-13 17:30:56 +0200

    Fixed styles with Prettier

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -7,20 +7,20 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
-        'primary': {
+        primary: {
           1000: '#aa5cdb',
           2000: '#090b1a',
           3000: '#1b1938',
         },
-        'neutral': {
+        neutral: {
           1000: '#ffffff',
           2000: 'rgba(255, 255, 255, 0.7)',
           3000: 'rgba(255, 255, 255, 0.6)',
         },
       },
       fontFamily: {
-        'inter': ['Inter', ...defaultTheme.fontFamily.sans],
-        'lexdec': ['Lexend Deca', ...defaultTheme.fontFamily.sans],
+        inter: ['Inter', ...defaultTheme.fontFamily.sans],
+        lexdec: ['Lexend Deca', ...defaultTheme.fontFamily.sans],
       },
       screens: {
         mob: '375px',
