7d847d5a0c22d4fe0d48591a7485bc281214b89d
senaid <senaid123>
2022-05-15 21:52:36 +0200

    Added final components and finished styling
    -imported images
    -added style to the components
    -imported fonts and applied colors

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'bg-col': '#19a2ae',
+        'very-dark': '#2d3248',
+        'drak-gray-blue': '#6a6f81',
+        'dark-gray': '#969696',
+      },
+      fontFamily: {
+        kumbhSans: ['Kumbh Sans', 'sans-serif'],
+      },
+      fontSize: {
+        head: '18px',
       },
     },
   },
