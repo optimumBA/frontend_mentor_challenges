4f55284179adef9be5fa27015191402e100fe5bc
Tarik Cahtarevic <tarikcahtarevic@gmail.com>
2022-05-17 16:47:09 +0200

    Inital commit

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./challenges/order_summary_component/index.html'],
   theme: {
@@ -5,6 +7,18 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        primary: {
+          'pale-blue': '#e0e8ff',
+          'bright-blue': '#3829e0',
+        },
+        neutral: {
+          'very-pale-blue': '#f5f7ff',
+          'desaturated-blue': '#7280a7',
+          'dark-blue': '#1f2f56',
+        },
+      },
+      fontFamily: {
+        'red-hat': ['Red Hat Display', ...defaultTheme.fontFamily.sans],
       },
     },
   },
