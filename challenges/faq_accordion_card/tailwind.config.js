3453cd2b6941dade03b4a047b679dc8e3e56325c
OmerSalkan <salkanovicomer@gmail.com>
2022-05-30 19:31:45 +0200

    Added images and font style

--- challenges/faq_accordion_card/tailwind.config.js
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'very-dark-desaturated-blue': 'hsl(238, 29%, 16%)',
+        'soft-red': 'hsl(14, 88%, 65%)',
+        'soft-violet': 'hsl(273, 75%, 66%)',
+        'soft-blue': 'hsl(240, 73%, 65%)',
+        'very-dark-grayish': 'blue: hsl(237, 12%, 33%)',
+        'dark-grayish-blue': 'hsl(240, 6%, 50%)',
+        'light-grayish-blue': 'hsl(240, 5%, 91%)'
       },
+      fontFamily: {
+        kumbh:['Kumbh Sans']
+      }
     },
   },
   plugins: [],
