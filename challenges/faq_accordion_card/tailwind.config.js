55779b6f46a7b768b08a1005b8a9ae2e4cab3718
senaid <senaid123>
2022-06-06 18:50:54 +0200

    add fonts and colors

--- challenges/faq_accordion_card/tailwind.config.js
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'text-dark': 'hsl(238, 29%, 16%)',
+        'text-red': 'hsl(238, 29%, 16%)',
+        'background-violet':  'hsl(273, 75%, 66%)',
+        'background-s-blue': 'hsl(240, 73%, 65%)',
+        'text-vd-g-blue': 'hsl(237, 12%, 33%)',
+        'text-d-g-blue': 'hsl(240, 6%, 50%)',
+        'devider': 'hsl(240, 5%, 91%)'
       },
+      fontFamily: {
+        kumbhSans: ['Kumbh Sans', 'sans-serif'],
+      },
+
     },
   },
   plugins: [],
