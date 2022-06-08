1e716ce9978b8e55b66f30aa85e18e89fc5a0e1b
OmerSalkan <salkanovicomer@gmail.com>
2022-06-08 22:50:48 +0200

    Fix source code link and import images and style

--- challenges/blogr_landing_page/tailwind.config.js
+++ challenges/blogr_landing_page/tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'lightRed': 'hsl(356, 100%, 66%)',/* cta text */
+        'verylLightRed': 'hsl(355, 100%, 74%)',/* CTA hover background*/
+        'veryDarkBlue':'hsl(208, 49%, 24%)',/* headlings */
+        'grayishBlue':'hsl(240, 2%, 79%)',/* footer text */
+        'veryDarkGrayishBlue':'hsl(207, 13%, 34%)',/* body copy*/
+        'veryDarkBlackBlue':'hsl(240, 10%, 16%)',/* footer background */
+        'mobileVeryLightRed':'hsl(13, 100%, 72%)',/* mobile gradient */
+        'mobileLighRred':'hsl(353, 100%, 62%)',/* mobile gradient */
+        'veryDarkGrayBlue':'hsl(237, 17%, 21%)',/* background gradient */
+        'veryDarkDesaturatedBlue':'hsl(237, 23%, 32%)',/* background gradient */
+
+
       },
+      fontFamily: {
+        overpass: ['Overpass'],
+        ubuntu: ['Ubuntu'],
+      },
+      backgroundImage: {
+        circles: "url('./images/bg-pattern-circles.svg')",
+        mobilePattern: "url('./images/bg-pattern-intro-mobile.svg')",
+        desktopPattern: "url('./images/bg-pattern-intro-desktop.svg')",
+      },
+      
     },
   },
   plugins: [],
