e9fc3c4cc70fee71f9e31610737b6e25ae5fb5ae
Tarik Cahtarevic <tarikcahtarevic@gmail.com>
2022-06-14 20:25:28 +0200

    Initial commit

--- challenges/faq_accordion_card/tailwind.config.js
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./challenges/faq_accordion_card/index.html'],
   theme: {
@@ -5,6 +7,28 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        primary: {
+          text: {
+            'vd-desat-blue': '#1d1e35',
+            'soft-red': '#f47c57',
+          },
+          gradient: {
+            'soft-violet': '#af68ea',
+            'soft-blue': '#6565e7',
+          },
+        },
+        neutral: {
+          text: {
+            'vd-grayish-blue': '#4a4b5e',
+            'dark-grayish-blue': '#787887',
+          },
+          dividers: {
+            'lite-grayish-blue': '#e7e7e9',
+          },
+        },
+      },
+      fontFamily: {
+        'kumbh-sans': ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
       },
     },
   },
