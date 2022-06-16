4057ba278303ee06123990241b7e5e49648feb66
Tarik Cahtarevic <tarikcahtarevic@gmail.com>
2022-06-16 14:04:11 +0200

    Initial commit

--- challenges/blogr_landing_page/tailwind.config.js
+++ challenges/blogr_landing_page/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./challenges/blogr_landing_page/index.html'],
   theme: {
@@ -5,6 +7,30 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        primary: {
+          'lite-red': '#ff525d',
+          'vlite-red': '#ff7a85',
+          'vdark-blue': '#1f3f5b',
+        },
+        neutral: {
+          'grayish-blue': '#c8c8cb',
+          'vdark-grayish-blue': '#4b5862',
+          'vdark-black-blue': '#25252d',
+        },
+        gradient: {
+          intro: {
+            'vl-red': '#ff8f70',
+            'lite-red': '#ff3d54',
+          },
+          body: {
+            'vdark-gray-blue': '#2c2d3f',
+            'vdark-desat-blue': '#3f4164',
+          },
+        },
+      },
+      fontFamily: {
+        overpass: [Overpass, ...defaultTheme.fontFamily.sans],
+        ubuntu: [Ubuntu, ...defaultTheme.fontFamily.sans],
       },
     },
   },
