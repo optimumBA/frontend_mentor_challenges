b11be1f047c2721871486c632ca831fa8cc65e90
OmerSalkan <salkanovicomer@gmail.com>
2022-06-18 22:33:12 +0200

    Finished header and mobile layot

--- challenges/blogr_landing_page/tailwind.config.js
+++ challenges/blogr_landing_page/tailwind.config.js
@@ -5,18 +5,16 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
-        'lightRed': 'hsl(356, 100%, 66%)',/* cta text */
-        'verylLightRed': 'hsl(355, 100%, 74%)',/* CTA hover background*/
-        'veryDarkBlue':'hsl(208, 49%, 24%)',/* headlings */
-        'grayishBlue':'hsl(240, 2%, 79%)',/* footer text */
-        'veryDarkGrayishBlue':'hsl(207, 13%, 34%)',/* body copy*/
-        'veryDarkBlackBlue':'hsl(240, 10%, 16%)',/* footer background */
-        'mobileVeryLightRed':'hsl(13, 100%, 72%)',/* mobile gradient */
-        'mobileLighRred':'hsl(353, 100%, 62%)',/* mobile gradient */
-        'veryDarkGrayBlue':'hsl(237, 17%, 21%)',/* background gradient */
-        'veryDarkDesaturatedBlue':'hsl(237, 23%, 32%)',/* background gradient */
-
-
+        lightRed: 'hsl(356, 100%, 66%)' /* cta text */,
+        veryLightRed: 'hsl(355, 100%, 74%)' /* CTA hover background*/,
+        veryDarkBlue: 'hsl(208, 49%, 24%)' /* headlings */,
+        grayishBlue: 'hsl(240, 2%, 79%)' /* footer text */,
+        veryDarkGrayishBlue: 'hsl(207, 13%, 34%)' /* body copy*/,
+        veryDarkBlackBlue: 'hsl(240, 10%, 16%)' /* footer background */,
+        mobileVeryLightRed: 'hsl(13, 100%, 72%)' /* mobile gradient */,
+        mobileLighRred: 'hsl(353, 100%, 62%)' /* mobile gradient */,
+        veryDarkGrayBlue: 'hsl(237, 17%, 21%)' /* background gradient */,
+        veryDarkDesaturatedBlue: 'hsl(237, 23%, 32%)' /* background gradient */,
       },
       fontFamily: {
         overpass: ['Overpass'],
@@ -26,8 +24,8 @@ module.exports = {
         circles: "url('./images/bg-pattern-circles.svg')",
         mobilePattern: "url('./images/bg-pattern-intro-mobile.svg')",
         desktopPattern: "url('./images/bg-pattern-intro-desktop.svg')",
+        prvasekcija: "url('./images/illustration-editor-desktop.svg')",
       },
-      
     },
   },
   plugins: [],
