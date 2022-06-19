3556ad0697ec585baedd3ac4e9a316616336bc7f
OmerSalkan <salkanovicomer@gmail.com>
2022-06-19 16:40:09 +0200

    Added images and style

--- challenges/interactive_pricing_component/tailwind.config.js
+++ challenges/interactive_pricing_component/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'soft-cyan': '#A5F3EB',
+        'strong-cyan': '#10D5C2',
+        'light-grayish-red': '#FEECE7',
+        'light-red': '#FF8C66',
+        'pale-blue':'#BDCCFF',
+        'very-pale-blue': 'hsl(230, 100%, 99%)',
+        'light-grayish-blue': 'hsl(224, 65%, 95%)',
+        'light-grayish-blue2': 'hsl(223, 50%, 87%)',
+        'grayish-blue': 'hsl(225, 20%, 60%)',
+        'dark-desaturated-blue': 'hsl(227, 35%, 25%)'
       },
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif']
+      }
     },
   },
   plugins: [],
