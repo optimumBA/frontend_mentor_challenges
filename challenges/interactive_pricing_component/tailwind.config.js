973b4e9f235feb1a7b0743cd16a2ec34dbb860b8
OmerSalkan <salkanovicomer@gmail.com>
2022-06-20 00:57:15 +0200

    Finished card layout

--- challenges/interactive_pricing_component/tailwind.config.js
+++ challenges/interactive_pricing_component/tailwind.config.js
@@ -9,16 +9,16 @@ module.exports = {
         'strong-cyan': '#10D5C2',
         'light-grayish-red': '#FEECE7',
         'light-red': '#FF8C66',
-        'pale-blue':'#BDCCFF',
+        'pale-blue': '#BDCCFF',
         'very-pale-blue': 'hsl(230, 100%, 99%)',
         'light-grayish-blue': 'hsl(224, 65%, 95%)',
         'light-grayish-blue2': 'hsl(223, 50%, 87%)',
         'grayish-blue': 'hsl(225, 20%, 60%)',
-        'dark-desaturated-blue': 'hsl(227, 35%, 25%)'
+        'dark-desaturated-blue': 'hsl(227, 35%, 25%)',
       },
       fontFamily: {
-        manrope: ['Manrope', 'sans-serif']
-      }
+        manrope: ['Manrope', 'sans-serif'],
+      },
     },
   },
   plugins: [],
