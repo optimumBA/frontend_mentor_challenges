22feda90ed7ddd4baef617e3612b5bc3b5df2541
senaid <senaid123>
2022-06-22 14:54:27 +0200

    add colors and fonts

--- challenges/interactive_pricing_component/tailwind.config.js
+++ challenges/interactive_pricing_component/tailwind.config.js
@@ -5,6 +5,35 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        /*Primary*/
+        /* Full slider bar*/
+        'soft-cyan': 'a5f3eb',
+        /*Slider background */
+        'strong-cyan': '#10d5c2',
+        /*Discount background */
+        'light-grayish-red': '#feece7',
+        /*Discount text */
+        'discount-text': '#ff8c66',
+        /*CTA text*/
+        'pale-blue': '#bdccff',
+
+        /*Neutral  */
+
+        /*Pricing component background */
+        'white': '#fffff',
+        /*Main background */
+        'very-pale-blue': 'fafbff',
+        /*Empty Slider Bar */
+        'light-grayish-blue': '#eaeefb',
+        /*Toggle background */
+        'light-grayish-blue': '#eaeefb',
+        /*Text*/
+        'grayish-blue': '858fad',
+        /*Text & CTA Background */
+        'dark-desaturated-blue': '#293356',
+      },
+      fontFamily:{
+        manrope: ['Manrope'],
       },
     },
   },
