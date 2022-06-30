1c7a1aa012e9a99b56fe938b18e08b78c16be48e
Tarik Cahtarevic <tarikcahtarevic@gmail.com>
2022-06-30 15:08:19 +0200

    Finish making desktop and mobile layout, add slider and button for discount

--- challenges/interactive_pricing_component/tailwind.config.js
+++ challenges/interactive_pricing_component/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./challenges/interactive_pricing_component/index.html'],
   theme: {
@@ -5,6 +7,23 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        primary: {
+          'soft-cyan': '#25f3eb',
+          'strong-cyan': '#10d5c2',
+          'lite-grayish-red': '#feece7',
+          'lite-red': '#ff8c66',
+          'pale-blue': '#bdccff',
+        },
+        neutral: {
+          'vpale-blue': '#fafbff',
+          'lite-grayish-blue': '#eaeefb',
+          'lite-grayish-blueII': '#cdd7ee',
+          'grayish-blue': '#858fad',
+          'd-desat-blue': '#293356',
+        },
+      },
+      fontFamily: {
+        manrope: ['Manrope', ...defaultTheme.fontFamily.sans],
       },
     },
   },
