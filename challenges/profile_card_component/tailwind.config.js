d8b36798cee2e9153d185ed1526d89e7f457065d
FarisGoralija <farisg55@gmail.com>
2022-07-04 10:09:18 +0200

    New commit for push

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'very-dark-desaturted-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      },
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
       },
     },
   },
