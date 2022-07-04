4800f229f8cd1eaf5e4e6140b5e1ae45a561aeaa
FarisGoralija <farisg55@gmail.com>
2022-07-04 19:29:23 +0200

    order summary component commit

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'bg-col': '#e0e8ff' /*pale blue*/,
+        'bg-button': '#3829e0' /*bright blue*/,
+        'bg-head': '#1f2f56' /*Dark blue*/,
+        'bg-text': '#7280a7' /*desaturated blue*/,
+        'bg-hover': '#1f2f56' /*Dark blue*/,
+        'bg-hover-button': '#f5f7ff' /*very pale blue*/,
+      },
+      fontFamily: {
+        redHatDisplay: ['Red Hat Display', 'sans-serif'],
       },
     },
   },
