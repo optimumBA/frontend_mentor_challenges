a9ea2538b6622a4cb44d2731f4487b9edd277ebb
amos-kibet <kibetamos97@gmail.com>
2022-11-23 00:53:43 +0300

    Configure Tailwind
    Used provided style-guide to configure Tailwind CSS

---
+++ challenges/qr_code_component/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./challenges/qr_code_component/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)',
+      },
+      fontFamily: {
+        outfit: ['Outfit'],
+      },
+    },
+  },
+  plugins: [],
+  variants: {},
+}
