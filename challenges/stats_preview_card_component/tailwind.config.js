40d0528070b0be887240bafa584ab5d1eb1971fe
Nittin Shankar <nittin.shankar@gmail.com>
2022-12-05 15:01:30 +0530

    Use colors from style guide

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+
+        'very-dark-blue': '#090B1A',
+        'dark-desaturated-blue': '#1B1938',
+        'soft-violet': '#AA5CDB'
       },
     },
   },
