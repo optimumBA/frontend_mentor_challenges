359def060fc35d4c4f050a1f108c9422738419b2
Nittin Shankar <nittin.shankar@gmail.com>
2022-12-07 15:02:43 +0530

    Add google fonts and polish the layout

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./challenges/profile_card_component/index.html'],
   theme: {
     extend: {
+      fontFamily: {
+        'kumbh-sans': ['Kumbh Sans']
+      },
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
