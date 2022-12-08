bb71a423506bb4f8cf420bc0f97cce29fd169b67
Nittin Shankar <nittin.shankar@gmail.com>
2022-12-08 09:47:12 +0530

    Add the optimum colors in configuration

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
       colors: {
         'grayish-blue': '#7B879D',
         'light-gray': '#D6E2F0',
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
       },
       fontFamily: {
         outfit: ['"Outfit"'],
