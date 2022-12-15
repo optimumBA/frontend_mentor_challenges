75fbd7342377970e43743e79aea74e54491314d0
Nittin Shankar <nittin.shankar@gmail.com>
2022-12-15 16:37:11 +0530

    Add gradient to section

--- challenges/blogr_landing_page/tailwind.config.js
+++ challenges/blogr_landing_page/tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
         'very-dark-blue': 'hsl(208, 49%, 24%)',
         'very-dark-grayish-blue': 'hsl(207, 13%, 34%)',
         'very-dark-black-blue': 'hsl(240, 10%, 16%)',
-        'grayish-blue': 'hsl(240, 2%, 79%)'
+        'grayish-blue': 'hsl(240, 2%, 79%)',
+        'very-dark-gray-blue': 'hsl(237, 17%, 21%)',
+        'very-dark-desaturated-blue': 'hsl(237, 23%, 32%)'
       },
     },
   },
