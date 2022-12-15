7dc4dea845ba132381fb450f6eb315a5837b4ae7
Nittin Shankar <nittin.shankar@gmail.com>
2022-12-15 16:21:53 +0530

    Create base design for the layout

--- challenges/blogr_landing_page/tailwind.config.js
+++ challenges/blogr_landing_page/tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'very-light-red': 'hsl(13, 100%, 72%)',
+        'light-red': 'hsl(353, 100%, 62%)',
+        'very-dark-blue': 'hsl(208, 49%, 24%)',
+        'very-dark-grayish-blue': 'hsl(207, 13%, 34%)',
+        'very-dark-black-blue': 'hsl(240, 10%, 16%)',
+        'grayish-blue': 'hsl(240, 2%, 79%)'
       },
     },
   },
