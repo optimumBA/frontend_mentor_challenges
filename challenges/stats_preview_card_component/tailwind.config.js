8a45a27dda9b8eba9310f16370567659be64438d
kagure-nyakio <kagurenyakio@gmail.com>
2023-01-11 18:12:49 +0300

    Stats Preview Page

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'optimum-blue': '#009efc',
-        'optimum-darkblue': '#0389e1',
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+        white: 'hsl(0, 0%, 100%)',
+        'transparent-white-main': 'hsla(0, 0%, 100%, 0.75)',
+        'transparent-white': 'hsla(0, 0%, 100%, 0.6)',
+      },
+      text: {
+        base: '15px',
+      },
+      fontFamily: {
+        main: ['Inter', 'sans-serif'],
+        titles: ['Lexend Deca', 'sans-serif'],
       },
     },
   },
