62bcd53c0434b3f604177b7a96be4a813c7d7ff2
kagure-nyakio <kagurenyakio@gmail.com>
2023-01-11 05:30:46 +0300

    Build QR Code Component

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -1,13 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./challenges/qr_code_component/index.html'],
   theme: {
     extend: {
       colors: {
-        'optimum-blue': '#009efc',
-        'optimum-darkblue': '#0389e1',
+        white: 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)',
+      },
+      fontFamily: {
+        display: ['Outfit', 'sans-serif'],
+      },
+      text: {
+        base: '15px',
       },
     },
   },
   plugins: [],
-  variants: {},
 }
