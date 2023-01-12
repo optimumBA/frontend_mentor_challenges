1716515dd94b2acda86688d5241679e2e851f6bb
kagure-nyakio <kagurenyakio@gmail.com>
2023-01-12 23:04:02 +0300

    Build the order summary component

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
+      },
+      fontFamily: {
+        base: ['Red Hat Display', 'sans-serif'],
+      },
+      boxShadow: {
+        btn: '0 20px 20px 0 hsla(245, 75%, 52%, 20%)',
+      },
+      backgroundImage: {
+        mobile: "url('pattern-background-mobile.svg')",
+        desktop: "url('pattern-background-desktop.svg')",
       },
     },
   },
