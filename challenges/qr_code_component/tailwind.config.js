95e578e8d05d2ce4cdc2b004d1bba0ed8e76775c
Rowa <jrowah@gmail.com>
2023-05-09 16:13:11 +0300

    set up solution env

---
+++ challenges/qr_code_component/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./challenges/qr_code_component/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+        lightGray: 'hsl(212, 45%, 89%)',
+      },
+    },
+  },
+  plugins: [],
+  variants: {},
+}
