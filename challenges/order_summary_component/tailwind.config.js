76e07a08845f18fc613e9c1e54b8abf5d431a140
Ayiko Fred <lgmfred@ayikoyo.com>
2023-08-13 09:30:27 +0300

    Finish basic design

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
+      },
+      fontFamily: {
+        'red-hat-display': ['Red Hat Display', 'sans-serif'],
       },
     },
   },
