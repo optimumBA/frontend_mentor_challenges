d90e12d93556e93c3d7a0936b22cb369fa0e58ad
Ayiko Fred <lgmfred@ayikoyo.com>
2023-08-22 17:24:33 +0300

    Finish foreground styling

--- challenges/chat_app_css_illustration/tailwind.config.js
+++ challenges/chat_app_css_illustration/tailwind.config.js
@@ -18,10 +18,11 @@ module.exports = {
         'light-magenta': 'hsl(293, 100%, 63%)',
         'light-violet': 'hsl(264, 100%, 61%)',
         // Secondary
-        'white': 'hsl(0, 0%, 100%)',
+        white: 'hsl(0, 0%, 100%)',
         'light-grayish-violet': 'hsl(270, 20%, 96%)',
         'very-dark-desaturated-violet': 'hsl(271, 36%, 24%)',
         'very-light-magenta': 'hsl(289, 100%, 72%)',
+        'left-text-bg': '#ede5f9',
       },
       fontFamily: {
         rubik: ['Rubik', 'sans-serif'],
