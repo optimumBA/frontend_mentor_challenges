c89d9567fc48f651811db270d4fa659af1ceaecb
Ayiko Fred <lgmfred@ayikoyo.com>
2023-09-22 08:13:56 +0300

    Add the basic HTML

--- challenges/blogr_landing_page/tailwind.config.js
+++ challenges/blogr_landing_page/tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'light-red-CTA-text': 'hsl(356, 100%, 66%)',
+        'very-light-red': 'hsl(355, 100%, 74%)',
+        'very-dark-blue': 'hsl(208, 49%, 24%)',
+        // Neutral
+        'white-text': 'hsl(0, 0%, 100%)',
+        'grayish-blue': 'hsl(240, 2%, 79%)',
+        'very-dark-grayish-blue': 'hsl(207, 13%, 34%)',
+        'very-dark-black-blue': 'hsl(240, 10%, 16%)',
+        // Background gradient - Intro/CTA mobile nav:
+        'very-light-red': 'hsl(13, 100%, 72%)',
+        'light-red': 'hsl(353, 100%, 62%)',
+        // Background gradient - body:
+        'very-dark-gray-blue': 'hsl(237, 17%, 21%)',
+        'very-dark-desaturated-blue': 'hsl(237, 23%, 32%)',
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        overpass: ['Overpass', 'sans-serif'],
       },
     },
   },
