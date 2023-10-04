5acfc2b8fe13b3787cc6cb390da788acd0df7aa2
ngumokenneth <ngumokenneth97@gmail.com>
2023-10-04 16:42:15 +0300

    added the custom fonts and colors

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+        'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+        white: 'hsl(0, 0%, 100%)',
+        'slightly-transparent-whites': 'hsla(0, 0%, 100%, 0.75)',
+        'slightly-transparent-white': 'hsla(0, 0%, 100%, 0.6)',
       },
     },
   },
