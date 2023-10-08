f5749b3532d389deb1053148a066e22904b81ea6
ngumokenneth <ngumokenneth97@gmail.com>
2023-10-08 14:23:24 +0300

    added the background pattern

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
       },
     },
   },
