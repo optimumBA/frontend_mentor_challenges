5e58c1f9c3c61c2c45bab924a6def293ed5ed209
ngumokenneth <ngumokenneth97@gmail.com>
2023-10-17 18:17:57 +0300

    first commit

--- challenges/blogr_landing_page/tailwind.config.js
+++ challenges/blogr_landing_page/tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'light-red': 'hsl(356, 100%, 66%)',
+        'very-light-red': 'hsl(355, 100%, 74%)',
+        'very-dark-blue': 'hsl(208, 49%, 24%)',
+        'app-white': 'hsl(0, 0%, 100%)',
+        'grayish-blue': 'hsl(240, 2%, 79%)',
+        'very-dark-grayish-blue': 'hsl(207, 13%, 34%)',
+        'very-dark-black-blue': 'hsl(240, 10%, 16%)',
+        // gradient
+        'very-light-red': 'hsl(13, 100%, 72%)',
+        'light-red': 'hsl(353, 100%, 62%)',
+        // background-gradient - body
+        'very-dark-gray-blue': 'hsl(237, 17%, 21%)',
+        'very-dark-desaturated-blue': 'hsl(237, 23%, 32%)',
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        overpass: ['Overpass', 'sans-serif'],
       },
     },
   },
