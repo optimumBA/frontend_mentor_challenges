fdfd1ab0f9521053aafa1c5396d0f014a8e7a711
Velma <akinyivee10@gmail.com>
2024-01-26 12:31:15 +0300

    edited html

---
+++ challenges/qr-code-component/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./challenges/qr_code_component/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+        White: 'hsl(0, 0%, 100%)',
+        'Light-gray': 'hsl(212, 45%, 89%)',
+        'Grayish-blue': 'hsl(220, 15%, 55%)',
+        'Dark-blue': 'hsl(218, 44%, 22%)',
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+  variants: {},
+}
