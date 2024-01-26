63b7ff69bf457c2b6c161717df628526b80a92a9
Velma <akinyivee10@gmail.com>
2024-01-26 08:42:44 +0300

    add href link

--- challenges/qr-code-component/tailwind.config.js
+++ challenges/qr-code-component/tailwind.config.js
@@ -5,12 +5,13 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
-        lightGray: 'hsl(212, 45%, 89%)',
-        grayishBlue: 'hsl(220, 15%, 55%)',
-        darkBlue: 'hsl(218, 44%, 22%)',
+        White: 'hsl(0, 0%, 100%)',
+        'Light-gray': 'hsl(212, 45%, 89%)',
+        'Grayish-blue': 'hsl(220, 15%, 55%)',
+        'Dark-blue': 'hsl(218, 44%, 22%)',
       },
       fontFamily: {
-        outfit: ['Outfit'],
+        outfit: ['Outfit', 'sans-serif'],
       },
     },
   },
