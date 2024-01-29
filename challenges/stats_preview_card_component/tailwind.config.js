67a36e055dbe669144790ea9e23d4530e6b3777f
Velma <akinyivee10@gmail.com>
2024-01-29 19:15:29 +0300

    stats preview card challenge

--- challenges/stats_preview_card_component/tailwind.config.js
+++ challenges/stats_preview_card_component/tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+        'white_heading-stats': 'hsl(0, 0%, 100%)',
+        'slightly-transparent-white-main': 'hsla(0, 0%, 100%, 0.75)',
+        'slightly-transparent-white-stat': 'hsla(0, 0%, 100%, 0.6)',
+      },
+      fontFamily: {
+        deca: ['Lexend Deca'],
+        inter: ['Inter'],
       },
     },
   },
